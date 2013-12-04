23673f4260263b12af7f325cf0f7b3009e0ef87a
imcloudwu <imcloudwu@gmail.com>
2013-12-04 14:53:32 +0800

    1.0.0.7

--- myTable/Form2.cs
+++ myTable/Form2.cs
@@ -694,11 +694,19 @@ namespace myTable
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            AccessHelper _A = new AccessHelper();
-            List<myTableUDT> UDTlist = _A.Select<myTableUDT>();
-            _A.DeletedValues(UDTlist); //清除UDT資料
-            dataGridViewX1.Rows.Clear();  //清除datagridview資料
-            LoadLastRecord(); //再次讀入Mapping設定
+            try
+            {
+                AccessHelper _A = new AccessHelper();
+                List<myTableUDT> UDTlist = _A.Select<myTableUDT>();
+                _A.DeletedValues(UDTlist); //清除UDT資料
+                dataGridViewX1.Rows.Clear();  //清除datagridview資料
+                LoadLastRecord(); //再次讀入Mapping設定
+            }
+            catch
+            {
+                MessageBox.Show("網路或資料庫異常,請稍後再試...");
+            }
+            
         }
     }
 
